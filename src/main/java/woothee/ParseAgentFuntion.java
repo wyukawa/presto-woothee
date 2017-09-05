b13c2c9e5bf8c16311917807647c06217c9e6bca
wyukawa <wyukawa@gmail.com>
2017-09-05 10:56:35 +0900

    fix https://github.com/prestodb/presto/issues/8903

--- src/main/java/woothee/ParseAgentFuntion.java
+++ src/main/java/woothee/ParseAgentFuntion.java
@@ -1,13 +1,13 @@
 package woothee;
 
+import com.facebook.presto.spi.PageBuilder;
 import com.facebook.presto.spi.block.Block;
 import com.facebook.presto.spi.block.BlockBuilder;
 import com.facebook.presto.spi.block.BlockBuilderStatus;
-import com.facebook.presto.spi.function.Description;
-import com.facebook.presto.spi.function.ScalarFunction;
-import com.facebook.presto.spi.function.SqlNullable;
-import com.facebook.presto.spi.function.SqlType;
+import com.facebook.presto.spi.function.*;
 import com.facebook.presto.spi.type.StandardTypes;
+import com.facebook.presto.spi.type.Type;
+import com.google.common.collect.ImmutableList;
 import io.airlift.slice.Slice;
 import io.airlift.slice.Slices;
 import is.tagomor.woothee.Classifier;
@@ -18,18 +18,34 @@ import java.util.Map;
 import static com.facebook.presto.spi.type.VarcharType.VARCHAR;
 
 public class ParseAgentFuntion {
+
+    private final PageBuilder pageBuilder;
+
+    public ParseAgentFuntion(@TypeParameter("map<varchar,varchar>") Type mapType) {
+        pageBuilder = new PageBuilder(ImmutableList.of(mapType));
+    }
+
     @ScalarFunction("parse_agent")
     @Description("Returns Map, which has keys such as 'category', 'name', 'os', 'version', 'vendor' and 'os_version'")
     @SqlType("map<varchar,varchar>")
-    public static Block parseAgent(@SqlNullable @SqlType(StandardTypes.VARCHAR) Slice slice) {
+    public Block parseAgent(@TypeParameter("map<varchar,varchar>") Type mapType, @SqlNullable @SqlType(StandardTypes.VARCHAR) Slice slice) {
         String argument = slice.toStringUtf8();
         Map<String, String> stringMap = Classifier.parse(argument);
-        BlockBuilder builder = VARCHAR.createBlockBuilder(new BlockBuilderStatus(), stringMap.size());
-        for (Map.Entry<String, String> entry : stringMap.entrySet()) {
-            VARCHAR.writeSlice(builder, Slices.utf8Slice(entry.getKey()));
-            VARCHAR.writeSlice(builder, Slices.utf8Slice(entry.getValue()));
+
+        if (pageBuilder.isFull()) {
+            pageBuilder.reset();
         }
-        return builder.build();
+
+        BlockBuilder blockBuilder = pageBuilder.getBlockBuilder(0);
+        BlockBuilder singleMapBlockBuilder = blockBuilder.beginBlockEntry();
+        for (Map.Entry<String, String> entry : stringMap.entrySet()) {
+            VARCHAR.writeSlice(singleMapBlockBuilder, Slices.utf8Slice(entry.getKey()));
+            VARCHAR.writeSlice(singleMapBlockBuilder, Slices.utf8Slice(entry.getValue()));
+        }
+        blockBuilder.closeEntry();
+        pageBuilder.declarePosition();
+
+        return (Block) mapType.getObject(blockBuilder, blockBuilder.getPositionCount() - 1);
     }
 
     @ScalarFunction("is_pc")
